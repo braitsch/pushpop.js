ef14100c6e48e88ea54c30a7eed16eb354445349
braitsch <stephen@braitsch.io>
2016-04-08 19:36:45 -0700

    demo page updates

--- public/javascripts/pushpop.js
+++ public/javascripts/pushpop.js
@@ -1,4 +1,22 @@
 
+/*
+    Copyright (C) 2016 Stephen Braitsch [http://braitsch.io]
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
 
 function PushPop()
 {
@@ -66,6 +84,7 @@ function PushPop()
 	var selectButton = $('.modal-push .btn-select');
 	var uploadButton = $('.modal-push .btn-upload');
 	var saveVideoButton = $('.modal-push .btn-save-video');
+	var thumbInstructions = $('.modal-push .instructions')
 
 	var image = {
 		width:0,
@@ -85,6 +104,7 @@ function PushPop()
 			videoPreview.hide();
 			progressbar.show();
 			thumbSettings.show();
+			if (mediaPreview.attr('src') != undefined) thumbInstructions.show();
 		}	else if (this.value.toLowerCase() == 'video'){
 			$('#image').hide();
 			$('#video').show();
@@ -92,6 +112,7 @@ function PushPop()
 			videoPreview.show();
 			progressbar.hide();
 			thumbSettings.hide();
+			thumbInstructions.hide();
 		}
 	});
 // open file dialog when the select button is clicked //
@@ -113,6 +134,7 @@ function PushPop()
 				imageInput.val(fileDialog.val().replace(/C:\\fakepath\\/i, ''));
 			}
 			reader.readAsDataURL(this.files[0]);
+			thumbInstructions.show();
 		}
 	});
 // setup the form to handle the image upload //
