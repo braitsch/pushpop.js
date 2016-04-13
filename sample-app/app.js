7c6b6b825788d609034252c9515bfd2a9eb80583
braitsch <stephen@braitsch.io>
2016-04-12 23:37:21 -0700

    resetting sample-app

--- sample-app/app.js
+++ sample-app/app.js
@@ -2,24 +2,17 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-// bodyParser only required for stripe integration //
-var bodyParser = require('body-parser'); 
 
 app.locals.pretty = true;
 app.set('port', process.env.PORT || 3000);
-app.set('view engine', 'jade');
-app.set('views', './server/views');
-// bodyParser only required for stripe integration //
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'pug');
+app.set('views', './server');
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/uploads'));
 
 require('./server/routes')(app);
-require('./server/stripe')(app);
 
 http.listen(app.get('port'), function(q, r)
 {
 	console.log('Express server listening on port', app.get('port'));
 });
-
--- sample-app/server/routes.js
+++ sample-app/server/routes.js
@@ -10,46 +10,17 @@ pushpop.uploadTo('uploads');
 // use mongodb as the database //
 pushpop.database('mongo');
 // save files to gcloud instead of the local filesystem //
-pushpop.service('gcloud', 'pushpop');
-
-// delete from demo anything that is older than three minutes //
-var CronJob = require('cron').CronJob;
-new CronJob('*/1 * * * *', pushpop.purge, 3, true, 'America/Los_Angeles');
+// pushpop.service('gcloud', 'pushpop');
 
 module.exports = function(app) {
 
 	app.get('/', function (req, res)
 	{	
-		res.redirect('/project/gallery');
+		res.render('index')
+		//res.sendfile(__dirname + '/index.html')
 	});
 
-	app.get('/project/:id', function(req, res)
-	{
-	// set the active project //
-		pushpop.setProject(req.params['id'], function(project){
-			res.render('gallery', { project : project });
-		});
-	});	
-
-	app.get('/project/:id/print', function(req, res){
-		pushpop.getProject(req.params['id'], function(project){
-			res.send({ project : project });
-		})
-	});
-
-	app.get('/print', function(req, res){
-		pushpop.getAll(function(projects){
-			res.send({ projects : projects });
-		})
-	});
-
-	app.get('/reset', function(req, res){
-		pushpop.reset(function(){
-			res.redirect('/');
-		});
-	});
-
-	app.post('/delete', pushpop.delete, function(req, res)
+	app.post('/upload', pushpop.upload, function(req, res)
 	{
 		if (!pushpop.error){
 			res.send('ok').status(200);
@@ -58,7 +29,7 @@ module.exports = function(app) {
 		}
 	});
 
-	app.post('/upload', pushpop.upload, function(req, res)
+	app.post('/delete', pushpop.delete, function(req, res)
 	{
 		if (!pushpop.error){
 			res.send('ok').status(200);
